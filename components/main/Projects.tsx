cbed079fdf84428391f56f8817c6ba1f137e8d0c
deji100 <adeyemideji9@gmail.com>
2026-01-12 10:01:14 +0100

    fix: update title

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
       id="projects"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        My Projects
+        Project Overview
       </h1>
       <div className="h-full w-full flex flex-row justify-center flex-wrap gap-10 px-10">
         <Link
